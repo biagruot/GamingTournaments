758b909f718f76d4ffe54c36016544f77ece62c8
Biagio Ruotolo <biagio.ruotolo@aboutyou.com>
2023-02-20 01:32:21 +0100

    feat: updates App with new components
    Updates the App with the new components. Splitted in header and main semantic HTML5 elements to improve accessibility.

--- src/index.tsx
+++ src/index.tsx
@@ -5,11 +5,26 @@ import GlobalStyle from './GlobalStyle';
 import store from './store';
 import Container from './components/Container';
 import H4 from './components/H4';
+import { Search } from './containers/search/Search';
+import { ActionsRow } from './components/ActionsRow';
+import { TournamentsList } from './containers/tournamentsList/TournamentsList';
+import { TournamentCreateBtn } from './containers/tournamentCreateBtn/TournamentCreateBtn';
+import { StatusInfo } from './containers/statusInfo/StatusInfo';
 
 const App = () => {
   return (
     <Container>
-      <H4>FACEIT Tournaments</H4>
+      <header>
+        <H4>FACEIT Tournaments</H4>
+        <ActionsRow>
+          <Search />
+          <TournamentCreateBtn />
+        </ActionsRow>
+      </header>
+      <main>
+        <StatusInfo />
+        <TournamentsList />
+      </main>
     </Container>
   );
 };
